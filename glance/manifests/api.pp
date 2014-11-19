119071dc7f4a7f2fe1049102b6415053e7549061
Martin Mágr <mmagr@redhat.com>
2014-11-19 13:58:54 -0500

    [glance] Add manage_service feature
    puppet-glance lacks of disabling service managing. This patch adds
    $manage_service parameter to relevant classes.
    
    Change-Id: I222b1a3318f5163f6ad1e39cbb8be10c440ab99f
    Closes-bug: #1359823
    
    Conflicts:
        glance/manifests/api.pp
        glance/manifests/registry.pp

--- glance/manifests/api.pp
+++ glance/manifests/api.pp
@@ -92,6 +92,10 @@
 #   (optional) User to authenticate as with keystone.
 #   Defaults to 'glance'.
 #
+# [*manage_service*]
+#   (optional) If Puppet should manage service startup / shutdown.
+#   Defaults to true.
+#
 # [*enabled*]
 #   (optional) Whether to enable services.
 #   Defaults to true.
@@ -167,6 +171,7 @@ class glance::api(
   $pipeline              = 'keystone+cachemanagement',
   $keystone_tenant       = 'services',
   $keystone_user         = 'glance',
+  $manage_service           = true,
   $enabled               = true,
   $sql_idle_timeout      = '3600',
   $sql_connection        = 'sqlite:///var/lib/glance/glance.sqlite',
@@ -393,10 +398,12 @@ class glance::api(
           '/etc/glance/glance-cache.conf']:
   }
 
-  if $enabled {
-    $service_ensure = 'running'
-  } else {
-    $service_ensure = 'stopped'
+  if $manage_service {
+    if $enabled {
+      $service_ensure = 'running'
+    } else {
+      $service_ensure = 'stopped'
+    }
   }
 
   service { 'glance-api':
